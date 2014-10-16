1201eb6f06cf8d14eeee2c435d4d271b2216b24a
nickhdamico <nick@nickdami.co>
2014-10-16 17:20:33 -0300

    back

--- classes/dbconfig.php
+++ classes/dbconfig.php
@@ -8,7 +8,7 @@
         }
 
         public function open_connection() {
-            $this->connection = mysql_connect(“thunqr.net”, “root”, “rednine9”);
+            $this->connection = mysql_connect("thunqr.net", "vennuit", "Greensix6");
             if (!$this->connection) {
                 die("Database connection failed: " . mysql_error());
             } else {
--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-	define (DB_USER, “root”);
-	define (DB_PASSWORD, “rednine9”);
-	define (DB_DATABASE, “vennuit”);
-	define (DB_HOST, “localhost”);
+	define (DB_USER, "vennuit");
+	define (DB_PASSWORD, "Greensix6");
+	define (DB_DATABASE, "vennuit");
+	define (DB_HOST, "thunqr.net");
 ?>
