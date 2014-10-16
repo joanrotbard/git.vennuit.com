ca0b72a9a502a99297715dee381cf421c6be7aaf
nickhdamico <nick@nickdami.co>
2014-10-16 13:58:22 -0300

    l;'

--- comenzar.php
+++ comenzar.php
@@ -36,11 +36,30 @@
 						
 							
                                 
-						<h4>Elegi lo que va en tu caja:</h4>
+						<h4>1.Elegi lo que va en tu caja:</h4>
 
 								<div class="row">
 									<div class="col-md-4">
 										<h1>Tampones</h1>
+								  </div>
+									<div class="col-md-4">
+										<h1>Toallitas</h1>
+									</div>
+									<div class="col-md-4">
+										<h1>Protectores Diarios</h1>
+									</div>
+                                    <br>
+<br><br><br><br>
+                                    <h4>1.Elegi lo que va en tu caja:</h4>
+								<div class="row">
+									<div class="col-md-4">
+										<p>Primer dia de ultima indisposicion:</p><input type='text' class="form-control" id='datetimepicker6'/>
+        </div>
+        <script type="text/javascript">
+            $(function () {
+                $('#datetimepicker6').datetimepicker();
+            });
+        </script>
 								  </div>
 									<div class="col-md-4">
 										<h1>Toallitas</h1>
@@ -74,75 +93,7 @@
 			<!-- CORE : end -->
 
 			<!-- BOTTOM PANEL : begin -->
-			<div id="bottom-panel">
-				<div class="bottom-panel-inner">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-6">
-
-								<!-- BOTTOM TEXT : begin -->
-								<div class="bottom-text various-content">
-
-									<h3>About Beautyspot</h3>
-									<p><strong>BEAUTYSPOT</strong> is an ideal template for <strong>beauty salon, hairdressers, wellness or spa</strong>. Clean code and top-notch web design techniques are wrapped with <strong>several gorgeous color schemes</strong> to choose from.</p><p>You can buy this responsive HTML5/CSS3 template on <a href="http://themeforest.net/user/ShakespeareThemes/portfolio?ref=LubosVolovar">ThemeForest</a>.</p>
-
-								</div>
-								<!-- BOTTOM TEXT : end -->
-
-							</div>
-							<div class="col-md-6">
-
-								<!-- BOTTOM SUBSCRIBE : begin -->
-								<div class="bottom-subscribe various-content">
-
-									<h3>Newsletter</h3>
-									<p>BEAUTYSPOT supports MailChimp integration.</p>
-									<form id="subscribe-form" action="http://volovar.us8.list-manage.com/subscribe/post-json?u=76a50c9454ec8ab78914d1bf2&amp;id=49e892f53d&amp;c=?" method="get">
-
-										<!-- VALIDATION ERROR MESSAGE : begin -->
-										<p style="display: none;" class="c-alert-message m-warning m-validation-error"><i class="ico fa fa-exclamation-circle"></i>Email address is required.</p>
-										<!-- VALIDATION ERROR MESSAGE : end -->
-
-										<!-- SENDING REQUEST ERROR MESSAGE : begin -->
-										<p style="display: none;" class="c-alert-message m-warning m-request-error"><i class="ico fa fa-exclamation-circle"></i>There was a connection problem. Try again later.</p>
-										<!-- SENDING REQUEST ERROR MESSAGE : end -->
-
-										<!-- SUCCESS MESSAGE : begin -->
-										<p style="display: none;" class="c-alert-message m-success"><i class="ico fa fa-check-circle"></i><strong>Sent successfully.</strong></p>
-										<!-- SUCCESS MESSAGE : end -->
-
-										<div class="form-fields">
-											<input class="m-required m-email" name="EMAIL" type="text" data-placeholder="Your email address" title="Your email address">
-											<button class="c-button submit-btn" type="submit" data-label="Subscribe" data-loading-label="Sending...">Subscribe</button>
-										</div>
-									</form>
-
-								</div>
-								<!-- BOTTOM SUBSCRIBE : end -->
-
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- BOTTOM PANEL : end -->
-
-			<!-- FOOTER : begin -->
-			<footer id="footer">
-				<div class="container">
-
-					<!-- FOOTER TWITTER : begin -->
-					<div class="footer-twitter m-paginated" data-id="LubosBudkovsky" data-limit="3" data-interval="12000">
-						<div class="footer-twitter-inner">
-							<i class="ico fa fa-twitter"></i>
-							<h4 class="twitter-title"><a href="https://twitter.com/LubosBudkovsky">@LubosBudkovsky</a></h4>
-							<div class="twitter-feed">
-								<span class="c-loading-anim"><span></span></span>
-							</div>
-						</div>
-					</div>
-					<!-- FOOTER TWITTER : end -->
-
+	
 					<!-- FOOTER BOTTOM : begin -->
 					<div class="footer-bottom">
 						<div class="row">
@@ -151,7 +102,7 @@
 								<!-- FOOTER MENU : begin -->
 								<nav class="footer-menu">
 									<ul>
-										<li><a href="index.php">Home</a></li>
+										<li><a href="index.php">inicio</a></li>
 										<li><a href="services.html">Services</a></li>
 										<li><a href="documentation.html">Documentation</a></li>
 									</ul>
