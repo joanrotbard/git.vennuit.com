c0af271d81f48fccc574f396850b8145bcee3c35
nickhdamico <nick@nickdami.co>
2014-10-16 13:43:40 -0300

    comenzar 1 test
    test

---
+++ comenzar.php
@@ -0,0 +1,280 @@
+﻿<!DOCTYPE html>
+<html>
+
+		<!-- HEADER : begin -->
+		<?php
+			include('menu.php');
+		?>
+		<!-- HEADER : end -->
+
+		<!-- WRAPPER : begin -->
+		<div class="col-md-6">
+		  <p><em>The Big Oxmox</em> advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+</div>
+		<div id="wrapper">
+
+			<!-- CORE : begin -->
+			<div id="core">
+
+				<!-- PAGE HEADER : begin -->
+				<div id="page-header">
+					<div class="container">
+						<h1>Comenzar</h1>
+						<ul class="breadcrumbs">
+							<li><a href="index.php">Inicio</a></li>
+							<li>Comenzar</li>
+						</ul>
+					</div>
+				</div>
+				<!-- PAGE HEADER : begin -->
+
+				<div class="container">
+
+					<!-- PAGE CONTENT : begin -->
+					<div id="page-content">
+					  <div class="various-content">
+
+							<!-- BASICS SECTION : begin -->
+						
+							
+                                
+						<h4>Elegi lo que va en tu caja:</h4>
+
+								<div class="row">
+									<div class="col-md-4">
+										<h1>Tampones</h1>
+								  </div>
+									<div class="col-md-4">
+										<h1>Toallitas</h1>
+									</div>
+									<div class="col-md-4">
+										<h1>Protectores Diarios</h1>
+									</div>
+						</div>
+
+								
+
+								
+								</div>
+						<!-- BASICS SECTION : end -->
+
+							<!-- LISTS SECTION : begin -->
+							<section>
+								<div class="row">
+									<div class="col-md-6">
+
+										<h2>Unordered List</h2>
+
+										<!-- UNORDERED LIST : begin -->
+										<ul>
+											<li>Far far away, behind the word mountains</li>
+											<li>far from the countries Vokalia and Consonantia</li>
+											<li>there live the blind texts
+												<ul>
+													<li>Far far away, behind the word mountains</li>
+													<li>far from the countries Vokalia and Consonantia</li>
+												</ul>
+											</li>
+											<li>Separated they live in Bookmarksgrove</li>
+										</ul>
+										<!-- UNORDERED LIST : end -->
+
+									</div>
+									<div class="col-md-6">
+
+										<h2>Ordered List</h2>
+
+										<!-- ORDERED LIST : begin -->
+										<ol>
+											<li>Far far away, behind the word mountains</li>
+											<li>far from the countries Vokalia and Consonantia</li>
+											<li>there live the blind texts
+												<ol>
+													<li>Far far away, behind the word mountains</li>
+													<li>far from the countries Vokalia and Consonantia</li>
+												</ol>
+											</li>
+											<li>Separated they live in Bookmarksgrove</li>
+										</ol>
+										<!-- ORDERED LIST : begin -->
+
+									</div>
+								</div>
+					  </section>
+							<!-- LISTS SECTION : end -->
+
+							<!-- TABLE SECTION : begin -->
+							<section>
+
+								<h2>Table</h2>
+
+								<!-- TABLE : begin -->
+								<table>
+									<thead>
+										<tr>
+											<th>Maecenas tempus</th>
+											<th>Condimentum</th>
+											<th>Adipiscing sem</th>
+											<th>Nullam quis ante</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Sed consequat, leo eget</td>
+											<td>Duis arcu tortor, suscipit eget</td>
+											<td>Phasellus ullamcorper ipsum</td>
+											<td>Curabitur ligula sapien, tincidunt</td>
+										</tr>
+										<tr>
+											<td>Phasellus ullamcorper ipsum</td>
+											<td>Curabitur ligula sapien, tincidunt</td>
+											<td>Sed consequat, leo eget</td>
+											<td>Duis arcu tortor, suscipit eget</td>
+										</tr>
+										<tr>
+											<td>Sed consequat, leo eget</td>
+											<td>Duis arcu tortor, suscipit eget</td>
+											<td>Phasellus ullamcorper ipsum</td>
+											<td>Curabitur ligula sapien, tincidunt</td>
+										</tr>
+										<tr>
+											<td>Phasellus ullamcorper ipsum</td>
+											<td>Curabitur ligula sapien, tincidunt</td>
+											<td>Sed consequat, leo eget</td>
+											<td>Duis arcu tortor, suscipit eget</td>
+										</tr>
+									</tbody>
+								</table>
+								<!-- TABLE : end -->
+
+							</section>
+							<!-- TABLE SECTION : end -->
+
+				  </div>
+			  </div>
+					<!-- PAGE CONTENT : end -->
+
+		  </div>
+
+</div>
+			<!-- CORE : end -->
+
+			<!-- BOTTOM PANEL : begin -->
+			<div id="bottom-panel">
+				<div class="bottom-panel-inner">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6">
+
+								<!-- BOTTOM TEXT : begin -->
+								<div class="bottom-text various-content">
+
+									<h3>About Beautyspot</h3>
+									<p><strong>BEAUTYSPOT</strong> is an ideal template for <strong>beauty salon, hairdressers, wellness or spa</strong>. Clean code and top-notch web design techniques are wrapped with <strong>several gorgeous color schemes</strong> to choose from.</p><p>You can buy this responsive HTML5/CSS3 template on <a href="http://themeforest.net/user/ShakespeareThemes/portfolio?ref=LubosVolovar">ThemeForest</a>.</p>
+
+								</div>
+								<!-- BOTTOM TEXT : end -->
+
+							</div>
+							<div class="col-md-6">
+
+								<!-- BOTTOM SUBSCRIBE : begin -->
+								<div class="bottom-subscribe various-content">
+
+									<h3>Newsletter</h3>
+									<p>BEAUTYSPOT supports MailChimp integration.</p>
+									<form id="subscribe-form" action="http://volovar.us8.list-manage.com/subscribe/post-json?u=76a50c9454ec8ab78914d1bf2&amp;id=49e892f53d&amp;c=?" method="get">
+
+										<!-- VALIDATION ERROR MESSAGE : begin -->
+										<p style="display: none;" class="c-alert-message m-warning m-validation-error"><i class="ico fa fa-exclamation-circle"></i>Email address is required.</p>
+										<!-- VALIDATION ERROR MESSAGE : end -->
+
+										<!-- SENDING REQUEST ERROR MESSAGE : begin -->
+										<p style="display: none;" class="c-alert-message m-warning m-request-error"><i class="ico fa fa-exclamation-circle"></i>There was a connection problem. Try again later.</p>
+										<!-- SENDING REQUEST ERROR MESSAGE : end -->
+
+										<!-- SUCCESS MESSAGE : begin -->
+										<p style="display: none;" class="c-alert-message m-success"><i class="ico fa fa-check-circle"></i><strong>Sent successfully.</strong></p>
+										<!-- SUCCESS MESSAGE : end -->
+
+										<div class="form-fields">
+											<input class="m-required m-email" name="EMAIL" type="text" data-placeholder="Your email address" title="Your email address">
+											<button class="c-button submit-btn" type="submit" data-label="Subscribe" data-loading-label="Sending...">Subscribe</button>
+										</div>
+									</form>
+
+								</div>
+								<!-- BOTTOM SUBSCRIBE : end -->
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- BOTTOM PANEL : end -->
+
+			<!-- FOOTER : begin -->
+			<footer id="footer">
+				<div class="container">
+
+					<!-- FOOTER TWITTER : begin -->
+					<div class="footer-twitter m-paginated" data-id="LubosBudkovsky" data-limit="3" data-interval="12000">
+						<div class="footer-twitter-inner">
+							<i class="ico fa fa-twitter"></i>
+							<h4 class="twitter-title"><a href="https://twitter.com/LubosBudkovsky">@LubosBudkovsky</a></h4>
+							<div class="twitter-feed">
+								<span class="c-loading-anim"><span></span></span>
+							</div>
+						</div>
+					</div>
+					<!-- FOOTER TWITTER : end -->
+
+					<!-- FOOTER BOTTOM : begin -->
+					<div class="footer-bottom">
+						<div class="row">
+							<div class="col-md-6 col-md-push-6">
+
+								<!-- FOOTER MENU : begin -->
+								<nav class="footer-menu">
+									<ul>
+										<li><a href="index.php">Home</a></li>
+										<li><a href="services.html">Services</a></li>
+										<li><a href="documentation.html">Documentation</a></li>
+									</ul>
+								</nav>
+								<!-- FOOTER MENU : end -->
+
+							</div>
+							<div class="col-md-6 col-md-pull-6">
+
+								<!-- FOOTER TEXT : begin -->
+								<div class="footer-text">
+									<p>Buy this template on <a href="http://themeforest.net/user/LubosVolovar/portfolio?ref=LubosVolovar">ThemeForest.net</a></p>
+								</div>
+								<!-- FOOTER TEXT : end -->
+
+							</div>
+						</div>
+					</div>
+					<!-- FOOTER BOTTOM : end -->
+
+				</div>
+			</footer>
+			<!-- FOOTER : end -->
+
+		</div>
+		<!-- WRAPPER : end -->
+
+		<!-- SCRIPTS : begin -->
+		<script src="library/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+		<script src="library/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+		<script src="library/js/jquery.ba-outside-events.min.js" type="text/javascript"></script>
+		<script src="library/js/owl.carousel.min.js" type="text/javascript"></script><!-- Carousel -->
+		<script src="library/js/jquery.magnific-popup.min.js" type="text/javascript"></script><!-- Lightbox -->
+		<script src="library/twitter/jquery.tweet.min.js" type="text/javascript"></script><!-- Twitter Feed -->
+		<script src="library/js/library.js" type="text/javascript"></script>
+		<script src="library/js/scripts.js" type="text/javascript"></script>
+		<!-- SCRIPTS : end -->
+
+	</body>
+</html>
